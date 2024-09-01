b99e87d03540df4b8a5c1ea5ea55f303fad93383
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-08-31 23:14:03 -0300

    feat: implements header inicial idea

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,17 @@
+import "./styles.css"
 export default function Header(){
     return(
         <header>
-            <h1>Ola mundo</h1>
+            <div className="titulo">
+                <h1>Pedro Henrique</h1>
+            </div>
+            <div className="links">
+                <a href=""><p>Projects</p></a>
+                <a href=""><p>About me</p></a>
+            </div>
+            <div className="contactButton">
+                <button>Contact</button>
+            </div>
         </header>
     )
 }
--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/header"
+import "./styles.css"
 export default function Home(){
     return(
         <div id="home">
